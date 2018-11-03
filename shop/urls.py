10664a81bfddc71dc12492871e90a8a853b0b621
Chinseok Lee <me@askcompany.kr>
2018-11-04 00:39:18 +0900

    formless 예시

--- shop/urls.py
+++ shop/urls.py
@@ -8,7 +8,9 @@ app_name = 'shop'
 
 urlpatterns = [
     path('archives/<yyyy:year>/', views.archives_year),
-    path('', views.item_list),
+    path('', views.item_list, name='item_list'),
     path('<int:pk>/', views.item_detail),
     # re_path(r'^(?P<pk>\d+)/$', views.item_detail),
+    path('item/new/', views.item_new),
 ]
+
