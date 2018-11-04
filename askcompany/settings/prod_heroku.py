c95588ea56649cb7b1a71e06c67ae138f2cd9de8
Chinseok Lee <me@askcompany.kr>
2018-11-04 23:52:29 +0900

    EP33: 다양한 데이터베이스 연동하기

--- askcompany/settings/prod_heroku.py
+++ askcompany/settings/prod_heroku.py
@@ -1,1 +1,12 @@
 from .common import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'qwer1234',
+        'HOST': '127.0.0.1',
+        # 'OPTIONS': {},
+    }
+}
