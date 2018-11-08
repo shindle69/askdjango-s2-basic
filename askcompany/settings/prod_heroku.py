bd4a53fd3ef74403d433f29b66831801e5fef1a5
Chinseok Lee <me@askcompany.kr>
2018-11-09 01:27:38 +0900

    EP34: Heroku를 통한 배포

--- askcompany/settings/prod_heroku.py
+++ askcompany/settings/prod_heroku.py
@@ -1,12 +1,4 @@
 from .common import *
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'qwer1234',
-        'HOST': '127.0.0.1',
-        # 'OPTIONS': {},
-    }
-}
+import django_heroku
+django_heroku.settings(locals())
