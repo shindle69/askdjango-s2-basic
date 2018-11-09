ea220524c379c662c510141e64fc4792aceb53ec
Chinseok Lee <me@askcompany.kr>
2018-11-09 11:31:07 +0900

    CSS 프레임워크 적용

--- askcompany/urls.py
+++ askcompany/urls.py
@@ -17,11 +17,14 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+# from django.views.generic import TemplateView
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls')),
     path('shop/', include('shop.urls')),
+    path('', RedirectView.as_view(pattern_name='shop:item_list'), name='root'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
