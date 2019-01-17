06c8832f963b08544e1e994a12f4a858346b876c
unknown <juyouso@gmail.com>
2019-01-17 16:51:40 +0900

    model 정의

--- blog/models.py
+++ blog/models.py
@@ -5,8 +5,10 @@ from askcompany.utils import uuid_upload_to
 class Post(models.Model):
     author_name = models.CharField(max_length=20)
     title = models.CharField(max_length=100)
-    content = models.TextField()
-    photo = models.ImageField(upload_to=uuid_upload_to)
+    gwaesa = models.TextField()    
+    danwal = models.TextField()
+    sangwal = models.TextField()    
+    mununwal = models.TextField()  
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
